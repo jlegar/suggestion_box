eec33d3891b07f97657d081d16d821cd511e9591
jlegar <juanlegar@gmail.com>
2018-08-27 17:45:29 +0200

    Add form functionality to add suggestions.

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,4 +1,19 @@
 app.controller('MainController', ['$scope', 'suggestions', function($scope, suggestions){
-  $scope.helloworld = 'Hello AngularJS!';
+  $scope.title = '';
   $scope.posts = suggestions.posts;
+  $scope.addSuggestion = function(){
+    if (!$scope.title || ($scope.title === '')) {
+      return;
+    }
+    const suggestion = {
+      title: $scope.title,
+      upvotes: 0,
+      comments: []
+    };
+    $scope.posts.push(suggestion);
+    $scope.title = '';
+  }
+  $scope.addVote = function (post) {
+    post.upvotes++;
+  }
 }])
