bdb76bc580aeb124ff4cf08506ca2ad562052981
jlegar <juanlegar@gmail.com>
2018-08-28 13:18:46 +0200

    Add Angular Route functionality and Home view.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
 var app = angular.module('SuggestionBox', []);
+
+app.config(function($routeProvider){
+  $routeProvider
+    .when('/', {
+      controller: 'MainController',
+      templateUrl: 'views/home.html'
+    })
+    .otherwise({
+      redirectTo: '/'
+    });
+});
