1346debbdb62616e0b5f0e9a3015e8fde1472b8f
Juan García <juanlegar@gmail.com>
2018-08-29 13:13:45 +0200

    Update SuggestionController.js

--- js/controllers/SuggestionController.js
+++ js/controllers/SuggestionController.js
@@ -1,8 +1,9 @@
-app.controller('SuggestionController', ['$scope', '$routeParams', 'suggestions', function($scope, $routeParams, suggestions){
-  $scope.post = suggestions.posts[$routeParams.id];
+app.controller('SuggestionController', ['$scope', '$routeParams', function($scope, $routeParams){
+  $scope.post = $scope.posts[$routeParams.id];
   $scope.comments = $scope.post.comments;
   $scope.newComment = '';
   $scope.addComment = function(){
     $scope.post.comments.push($scope.newComment);
+    $scope.newComment = '';
   };
 }])
